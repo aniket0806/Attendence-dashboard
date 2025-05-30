dc3a7b993960f4797ef2a848c842d33c54fc753c
aniket0806 <97946316+aniket0806@users.noreply.github.com>
2025-05-30 15:14:05 +0530

    updATE TIME LINIT

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -7,6 +7,9 @@ const pool = createPool({
     user: process.env.DB_USER,
     password:process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    waitForConnections: true,
+  connectionLimit: 5, // Keep this LOW for db4free.net
+  queueLimit: 0
 
 });
 
